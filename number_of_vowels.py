d3ec01849dea0d3e446e49d8c0c45e82f605a194
Aryan <109296614+itsaryan001@users.noreply.github.com>
2022-07-15 22:32:33 +0600

    Create number_of_vowels.py

---
+++ number_of_vowels.py
@@ -0,0 +1,19 @@
+def check_vowels():
+    text = input('Enter your text: ')
+    vowels = 0
+
+    for v in text:
+        if v == 'a' or v == 'e' or v == 'i' or v == 'o' or v == 'u':
+            vowels += 1
+
+    print(vowels)
+
+    repeat = input('Want to check another text? "y" or "n": ').lower()
+
+    if repeat == 'y':
+        check_vowels()
+    else:
+        exit()
+
+
+check_vowels()
